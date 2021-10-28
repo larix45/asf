a29623ee373875f35c69f3f364e449f39b87806a
Akr0nys <55049058+larix45@users.noreply.github.com>
2021-10-28 19:50:41 +0200

    le grande finale

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -8,13 +8,13 @@ import shuffle from '../hooks/randomizeArray';
 
 const Tab2: React.FC = () => {
   const randomize = async() => {
-    let vars = ["Ataki na Bartka", "Wracając...", "Coś nie działa", "Rozpakowanie", "To wam na schemacie pokaże", "Referat", "Pytanie retoryczne"]
+    let vars = ["Pedofilia", "Jebać Kobiety", "Jebanie Lewicy", "Cukerberg", "Losowy cytat", "WOS", "LGBT", "TikTok/Messenger", "Świat jest Chujowy", "Przemoc Fizyczna", "Przemoc Werbalna", "Alzheimer", "Picie na lekcji", "Pójście do Toalety", "'Róbta co Chceta'", "Prezentowanie na Uczniu", "Faktyczne Uczenie", "Głęboki wywód polityczny", "Ataki na Bartka", "Wracając...", "Coś nie działa", "Rozpakowywanie", "To wam na schemacie pokaże", "Referat", "Pytanie retoryczne"]
     //let vars = ["ad",2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
     vars = shuffle(vars)
     ReactDOM.render(<><IonButton id="bingoFieldBtn">  {vars[0]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[1]} </IonButton>  <IonButton id="bingoFieldBtn">  {vars[2]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[3]} </IonButton></>, document.getElementById("firstRow"));
     ReactDOM.render(<><IonButton id="bingoFieldBtn">  {vars[4]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[5]} </IonButton>  <IonButton id="bingoFieldBtn">  {vars[6]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[7]} </IonButton></>, document.getElementById("secondRow"));
-    ReactDOM.render(<><IonButton id="bingoFieldBtn">  {vars[8]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[9]} </IonButton>  <IonButton id="bingoFieldBtn">  {vars[10]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[11]} </IonButton></>, document.getElementById("thirdRow"));
-    ReactDOM.render(<><IonButton id="bingoFieldBtn">  {vars[12]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[13]} </IonButton>  <IonButton id="bingoFieldBtn">  {vars[14]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[15]} </IonButton></>, document.getElementById("forthRow"));
+    ReactDOM.render(<><IonButton id="bingoFieldBtn">  {vars[8]} </IonButton> <IonButton id="bingoFieldBtn">  {vars[9]} </IonButton>  <IonButton id="bingoFieldBtn">  {vars[10]}</IonButton> <IonButton id="bingoFieldBtn">  {vars[11]}</IonButton></>, document.getElementById("thirdRow"));
+    ReactDOM.render(<><IonButton id="bingoFieldBtn">  {vars[12]}</IonButton> <IonButton id="bingoFieldBtn">  {vars[13]}</IonButton>  <IonButton id="bingoFieldBtn">  {vars[14]}</IonButton> <IonButton id="bingoFieldBtn">  {vars[15]}</IonButton></>, document.getElementById("forthRow"));
   
   }
   return (
@@ -24,14 +24,13 @@ const Tab2: React.FC = () => {
           <IonTitle>Losowanie Bingo Biologia</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen scrollX scrollY>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonTabButton>
-          <IonContent scrollX scrollY>
+          <IonContent id="grid_content">
               <IonGrid fixed>
                 <IonRow id="firstRow">
                 </IonRow>
@@ -44,9 +43,8 @@ const Tab2: React.FC = () => {
               </IonGrid>
           </IonContent>
           <IonButton onClick={randomize} id="losowaniebtn" color="danger">
-          Losuj
-          </IonButton>
-        </IonTabButton>
+            Losuj
+           </IonButton>
       </IonContent>
     </IonPage>
   );
